b8e39a6a00754406cab4abd3b88e041e1b28d945
yoX64 <yox64@yahoo.com>
2014-05-05 11:37:05 +0300

    Error pages

--- applications/_control/errors/error_404.php
+++ applications/_control/errors/error_404.php
@@ -1,62 +1,82 @@
 <!DOCTYPE html>
-<html lang="en">
 <head>
-<title>404 Page Not Found</title>
-<style type="text/css">
+	<meta charset = "utf-8" >
+	<meta name = "robots" value = "noindex" >
+	<meta name="author" content="yoX64" />
 
-::selection{ background-color: #E13300; color: white; }
-::moz-selection{ background-color: #E13300; color: white; }
-::webkit-selection{ background-color: #E13300; color: white; }
+	<link href = "<?=base_url()?>/applications/_control/images/favicon.png" rel = "shortcut" >
+	<link href = "<?=base_url()?>applications/_control/css/main.css" type = "text/css" rel = "stylesheet" >
+	<link href = 'http://fonts.googleapis.com/css?family=Cuprum' rel = "stylesheet" type="text/css" >
 
-body {
-	background-color: #fff;
-	margin: 40px;
-	font: 13px/20px normal Helvetica, Arial, sans-serif;
-	color: #4F5155;
-}
+	<title>Easy Panel - 404 Page Not Found</title>
 
-a {
-	color: #003399;
-	background-color: transparent;
-	font-weight: normal;
-}
+	<script type="text/javascript" src = "<?=base_url()?>/applications/_control/js/jquery_1.7.1.min.js"></script>
 
-h1 {
-	color: #444;
-	background-color: transparent;
-	border-bottom: 1px solid #D0D0D0;
-	font-size: 19px;
-	font-weight: normal;
-	margin: 0 0 14px 0;
-	padding: 14px 15px 10px 15px;
-}
+	<!-- ckeditor -->
 
-code {
-	font-family: Consolas, Monaco, Courier New, Courier, monospace;
-	font-size: 12px;
-	background-color: #f9f9f9;
-	border: 1px solid #D0D0D0;
-	color: #002166;
-	display: block;
-	margin: 14px 0 14px 0;
-	padding: 12px 10px 12px 10px;
-}
+	<script type="text/javascript" src = "<?=base_url()?>/applications/_control/js/ckeditor/ckeditor.js"></script>
 
-#container {
-	margin: 10px;
-	border: 1px solid #D0D0D0;
-	-webkit-box-shadow: 0 0 8px #D0D0D0;
-}
+	<!-- -->
 
-p {
-	margin: 12px 15px 12px 15px;
-}
-</style>
+	<script type = "text/javascript" src = "<?=base_url()?>/applications/_control/js/forms/forms.js"></script>
+	<script type = "text/javascript" src = "<?=base_url()?>/applications/_control/js/forms/chosen.jquery.min.js"></script>
+
+	<script type = "text/javascript" src = "<?=base_url()?>/applications/_control/js/uploader/plupload.js" ></script>
+	<script type = "text/javascript" src = "<?=base_url()?>/applications/_control/js/uploader/plupload.html5.js" ></script>
+	<script type = "text/javascript" src = "<?=base_url()?>/applications/_control/js/uploader/plupload.html4.js" ></script>
+	<script type = "text/javascript" src = "<?=base_url()?>/applications/_control/js/uploader/jquery.plupload.queue.js "></script>
+
+	<script type = "text/javascript" src = "<?=base_url()?>/applications/_control/js/ui/jquery.tipsy.js" ></script>
+	<script type = "text/javascript" src = "<?=base_url()?>/applications/_control/js/ui/jquery.prettyPhoto.js" ></script>
+	<script type = "text/javascript" src = "<?=base_url()?>/applications/_control/js/ui/jquery.alerts.js"></script>
+
+	<script type = "text/javascript" src = "<?=base_url()?>/applications/_control/js/frontend_custom.js" ></script>
+
+	<script type = "text/javascript" >
+
+		var base_url 	= '{BASE_URL}';
+		var app_url 	= '{APP_URL}';
+
+	</script>
+
 </head>
+
 <body>
-	<div id="container">
-		<h1><?php echo $heading; ?></h1>
-		<?php echo $message; ?>
-	</div>
+
+<!-- Top navigation bar -->
+<div id="topNav">
+    <div class="fixed">
+        <div class="wrapper">
+            
+            <div class="fix"></div>
+        </div>
+    </div>
+</div>
+
+<!-- Error info area -->
+<div class="wrapper">
+    <div class="errorPage">
+        <h2 class="red errorTitle"><span>Something went wrong here</span></h2>
+        <h1>404</h1>
+        <span class="bubbles"></span>
+        <?php echo  $message; ?>
+        <div class="backToDash"><a href="<?=base_url()?>_control.php/dashboard" title="" class="seaBtn button">Back to Dashboard</a></div>
+    </div>
+</div>
+
+<div id="footer">
+
+			<div class="wrapper">
+
+				<span>
+					&copy; Copyright 2013.
+					Frontend by <a href="#" target="_blank">Eugene Kopyov</a> and
+					backend by <a href="http://www.yox64.com" target="_blank">yoX64</a>
+				</span>
+
+			</div><!-- .wrapper -->
+
+		</div><!-- #footer -->
+
 </body>
 </html>
