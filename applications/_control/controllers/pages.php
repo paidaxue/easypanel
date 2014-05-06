f7b9ebff5a35e58c40f4b8dc6948318053e4c679
Alex Radoi <axxwee@gmail.com>
2014-05-06 12:51:11 +0300

    Edit page sidebar options

--- applications/_control/controllers/pages.php
+++ applications/_control/controllers/pages.php
@@ -168,6 +168,8 @@ class Pages extends MY_Controller {
 
 		$page_info = $this->pages_model->get_page_by_id( $id_page );
 
+		$sidebars = $this->pages_model->get_sidebars();
+
 		$page_type = $this->pages_model->get_parents_by_id( $id_page );
 		foreach( $page_type as $type ) {
 
@@ -201,22 +203,32 @@ class Pages extends MY_Controller {
 		$page_title = $this->lang->line('pages_edit_page') . $page_info->title;
 
 		$content = $this->parser->parse( $filenames[ 'edit_page' ], array(
-																																	'PAGE_TYPE' 										=> $page_type,
-																																	'MODULES' 											=> $modules,
-																																	'title' 												=> $page_info->title,
-																																	'id_page' 											=> $page_info->id_page,
-																																	'content' 											=> $page_info->content,
-																																	'lang_edit_page' 								=> $this->lang->line('pages_edit_page'),
-																																	'lang_title_field' 							=> $this->lang->line('pages_title_field'),
-																																	'lang_content_type_field' 			=> $this->lang->line('pages_content_type_field'),
-																																	'lang_content_field' 						=> $this->lang->line('pages_content_field'),
-																																	'lang_menu_options' 						=> $this->lang->line('pages_menu_options'),
-																																	'lang_page_type_field' 					=> $this->lang->line('pages_page_type_field'),
-																																	'lang_default_page_type_value' 	=> $this->lang->line('pages_default_page_type_value'),
-																																	'lang_empty_page_type_value' 		=> $this->lang->line('pages_empty_page_type_value'),
-																																	'lang_required_fields' 					=> $this->lang->line('error_required_fields'),
-																																	'lang_submit_form' 							=> $this->lang->line('pages_submit_form'),
-																																), true );
+				'PAGE_TYPE' 										=> $page_type,
+				'MODULES' 											=> $modules,
+				'SIDEBARS'											=> $sidebars,
+				'title' 												=> $page_info->title,
+				'id_page' 											=> $page_info->id_page,
+				'content' 											=> $page_info->content,
+				'lang_edit_page' 								=> $this->lang->line('pages_edit_page'),
+				'lang_title_field' 							=> $this->lang->line('pages_title_field'),
+				'lang_content_type_field' 			=> $this->lang->line('pages_content_type_field'),
+				'lang_content_field' 						=> $this->lang->line('pages_content_field'),
+				'lang_menu_options' 						=> $this->lang->line('pages_menu_options'),
+				'lang_page_type_field' 					=> $this->lang->line('pages_page_type_field'),
+				'lang_default_page_type_value' 	=> $this->lang->line('pages_default_page_type_value'),
+				'lang_empty_page_type_value' 		=> $this->lang->line('pages_empty_page_type_value'),
+				'lang_required_fields' 					=> $this->lang->line('error_required_fields'),
+				'lang_submit_form' 							=> $this->lang->line('pages_submit_form'),
+				'lang_sidebars_style' 							=> $this->lang->line('pages_sidebars_style'),
+						'lang_sidebar_none' 							=> $this->lang->line('pages_sidebar_none'),
+						'lang_sidebar_left' 							=> $this->lang->line('pages_sidebar_left'),
+						'lang_sidebar_right' 							=> $this->lang->line('pages_sidebar_right'),
+						'lang_sidebar_both' 							=> $this->lang->line('pages_sidebar_both'),
+						'lang_sidebar_options' 							=> $this->lang->line('pages_sidebars_option'),
+						'lang_sidebar_name' 							=> $this->lang->line('pages_sidebar_name'),
+						'lang_sidebar_name_left' 						=> $this->lang->line('pages_sidebar_name_left'),
+						'lang_sidebar_name_right' 						=> $this->lang->line('pages_sidebar_name_right')
+			), true );
 
 		$page = page_builder( 'header', $page_title, 'body', 'body_header', 'top_nav', 'body_content', $content );
 		$this->parser->parse( 'base_template', $page );
@@ -231,7 +243,28 @@ class Pages extends MY_Controller {
 		$pages[ 'title' ] = $this->input->post( 'title', true );
 		$pages[ 'page_type' ] = $this->input->post( 'page_type' );
 		$pages[ 'module' ] = $this->input->post( 'modules' );
-		$pages[ 'content' ] = $this->input->post( 'content' );
+		$pages[ 'content' ] = $this->input->post( 'editor1' );
+		$pages[ 'sidebar_style'] = $this->input->post('sidebar_style');
+
+		if($pages['sidebar_style'] == 'none') {
+			$pages['sidebar_left'] = '0';
+			$pages['sidebar_right'] = '0';
+		}
+
+		if($pages['sidebar_style'] == 'left') {
+			$pages['sidebar_left'] = $this->input->post('sidebar_left');
+			$pages['sidebar_right'] = '0';
+		}
+
+		if($pages['sidebar_style'] == 'right') {
+			$pages['sidebar_left'] = '0';
+			$pages['sidebar_right'] = $this->input->post('sidebar_right');
+		}
+
+		if($pages['sidebar_style'] == 'both') {
+			$pages['sidebar_left'] = $this->input->post('sidebar_left');
+			$pages['sidebar_right'] = $this->input->post('sidebar_right');
+		}
 
 		if( $pages[ 'module' ] == 'homepage' ) {
 
--- applications/_control/views/pages/edit_pages.php
+++ applications/_control/views/pages/edit_pages.php
@@ -48,7 +48,7 @@
       
       	<label id="ckeditor">{lang_content_field}</label><br />
         <p id="ckeditor-arange">
-          <textarea class="ckeditor" cols="80" id="editor1" name="editor1" rows="10"> </textarea>
+          <textarea class="ckeditor" cols="80" id="editor1" name="editor1" rows="10">{content}</textarea>
         </p>
 
       </div><!-- #normal_content -->
@@ -84,6 +84,47 @@
       	<div class="fix"></div>
 
      	</div><!-- .rowElem -->
+
+    </div><!-- .widget -->
+
+    <div class = "widget">
+      <div class="head">
+
+        <h5>{lang_sidebar_options}</h5>
+
+      </div><!-- .widget -->
+      <div class="rowElem">
+
+        <label class = "sidebar_style"> * {lang_sidebars_style}</label>
+        <div class="formRight noSearch">
+          <select name="sidebar_style" class = "chzn-select"  id = "sidebar_style" >
+              <option value="none">{lang_sidebar_none}</option>
+              <option value="left">{lang_sidebar_left}</option>
+              <option value="right">{lang_sidebar_right}</option>
+              <option value="both">{lang_sidebar_both}</option>
+          </select>
+        </div>
+
+        <label class = "sidebar_style left"> * {lang_sidebar_name_left}</label>
+        <div class="formRight noSearch" id="sidebar_name_left">
+          <select name="sidebar_left" class = "chzn-select"  id = "sidebar_name_left" >
+              {SIDEBARS}
+                <option value="{id_sidebar}">{name}</option>
+              {/SIDEBARS}
+          </select>
+        </div>
+
+        <label class = "sidebar_style right"> * {lang_sidebar_name_right}</label>
+        <div class="formRight noSearch" id="sidebar_name_right">
+          <select name="sidebar_right" class = "chzn-select"   >
+              {SIDEBARS}
+                <option value="{id_sidebar}">{name}</option>
+              {/SIDEBARS}
+          </select>
+        </div>
+        <div class="fix"></div>
+
+      </div><!-- .rowElem -->
 
     </div><!-- .widget -->
 
